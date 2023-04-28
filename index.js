855884f57ba1ac1d84097c3c5d59ca1f0401f23e
valentyna1234 <pisareva.valya@gmail.com>
2023-04-29 01:29:25 +0300

    Add index.js file

---
+++ index.js
@@ -0,0 +1,53 @@
+//Це ladder (сходи) – об'єкт, який дозволяє підніматися вгору та спускатися:
+
+//let ladder = {
+//    step: 0,
+//    up: function () {
+//        this.step++;
+//    },
+//    down: function () {
+//        this.step--;
+//    },
+//    showStep: function () { // показывает текущую ступеньку
+//        alert(this.step);
+//    }
+//};
+//Тепер, якщо нам потрібно зробити кілька послідовних викликів, 
+//ми можемо виконати це так:
+
+//ladder.up();
+//ladder.up();
+//ladder.down();
+//ladder.showStep(); // 1
+
+//Змініть код методів up, down і showStep таким Таким чином, 
+//щоб їх виклик можна було зробити по ланцюжку, наприклад:
+
+//ladder.up().up().down().showStep(); // 1
+
+
+
+let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
+    },
+    down() {
+      this.step--;
+      return this;
+    },
+    showStep() {
+      console.log( this.step );
+      return this;
+    }
+  };
+
+ladder
+  .up()
+  .up()
+  .down()
+  .showStep()
+console.log(ladder);
+
+
